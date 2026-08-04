d3cbc710848d5428c8f7f1320d55b1f4dbf5da7a
garado <alexisgarado@gmail.com>
2026-08-03 20:10:01 -0500

    chore: rename package to garado.dev.transit

--- tool/src/main/kotlin/dev/garado/transit/HomeScreen.kt
+++ tool/src/main/kotlin/dev/garado/transit/HomeScreen.kt
@@ -1,4 +1,4 @@
-package dev.garado.template
+package dev.garado.transit
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
--- tool/src/main/kotlin/dev/garado/transit/ToolEntryPoint.kt
+++ tool/src/main/kotlin/dev/garado/transit/ToolEntryPoint.kt
@@ -1,4 +1,4 @@
-package dev.garado.template
+package dev.garado.transit
 
 import android.util.Log
 import com.thelightphone.sdk.EntryPoint
